b67814d9ae55a707d2ac784f007bc9d612252ddb
ming.gu <ming.gu@gearradio.net>
2022-01-09 19:53:11 +0800

    Initial Commit

---
+++ RecipeDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeDemo
+//
+//  Created by  明智 on 2022/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeDemo/RecipeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeDemoApp.swift
+//  RecipeDemo
+//
+//  Created by  明智 on 2022/1/9.
+//
+
+import SwiftUI
+
+@main
+struct RecipeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
